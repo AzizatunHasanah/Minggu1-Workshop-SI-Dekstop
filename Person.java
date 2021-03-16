e2caba78e41b25faf69e06df4dbb099bc2803742
AzizatunHasanah <74762009+AzizatunHasanah@users.noreply.github.com>
2021-03-16 23:54:49 +0700

    E41200681_Azizatun Hasanah_Gol A BWS

---
+++ Person.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author 62896
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String lName  = "Palombo";
+        int stuId = 123456789;
+        String stuStatus = "Active";
+        
+        System.out.println ("Student Id : " + stuId);
+        System.out.print("Studet Name : " + fName);
+        System.out.println(" " + lName);
+        System.out.println("Student Status : " + stuStatus);
+        
+    }
+    
+}
